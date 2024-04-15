c2b5a4fc7a4cdb92f74819633869324da921edc0
thabith16 <kstyahm@gmail.com>
2024-04-15 16:26:45 +0530

    checkbox

--- lib/pages/Home_Page.dart
+++ lib/pages/Home_Page.dart
@@ -14,6 +14,18 @@ class HomePage extends StatefulWidget {
 
 
 class _HomePageState extends State<HomePage> {
+
+//list of todo tasks
+  List toDoList = [
+    ["Make Tutorial", false],
+    ["Do Excercise", false],
+  ];
+
+  // checkbox was tapped
+  void checkBoxChanged() {
+    
+  }
+
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,10 +34,18 @@ class _HomePageState extends State<HomePage> {
         title: Text('To Do App'),
         elevation: 0,
       ),
-      body: ListView(
-        children: [
-          ToDoTile()
-        ],
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0], 
+            taskCompleted: toDoList[index][1], 
+            onChanged: (value) => checkBoxChanged,
+            
+            );
+
+        },
+
       ),
       );
     
